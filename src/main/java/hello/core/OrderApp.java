9e70af9159c86266751a9cb494b752006aea1f4f
JaeMin <dnfktmzm@naver.com>
2024-05-19 23:13:51 +0900

    Spring Bean으로 변경해서 적용하기

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -3,18 +3,22 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
-import hello.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
-        Appconfig appconfig = new Appconfig();
-        MemberService memberService = appconfig.memberService();
-        OrderService orderService = appconfig.orderService();
+//        AppConfig appconfig = new AppConfig();
+//        MemberService memberService = appconfig.memberService();
+//        OrderService orderService = appconfig.orderService();
         //MemberService memberService = new MemberServiceImpl();
         //OrderService orderService = new OrderServiceImpl();
+
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        OrderService orderService = ac.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
